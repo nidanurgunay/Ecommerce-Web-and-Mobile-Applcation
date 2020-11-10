3d22b544e0ae4ac50a17612ed7038a83bdb09a8b
Rabia Aydemir <rabiaaydemir@sabanciuniv.edu>
2020-11-11 00:20:42 +0300

    Work in progress

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+
+const users = [];
+
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: "Kyle" });
+});
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+app.post("/login", (req, res) => {});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+app.post("/register", (req, res) => {
+  try {
+    const hashedPassword = bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hasjedPassword,
+    });
+    res.redirect("/login");
+  } catch {
+    res.redirect.apply("/register");
+  }
+  console.log(users);
+});
+app.listen(3000);
