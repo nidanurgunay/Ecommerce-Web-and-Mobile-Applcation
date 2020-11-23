bed6140de291135c2257684d9d8a4a244046161d
Nidanur Günay <nidanur@sabanciuniv.edu>
2020-11-23 08:55:23 +0300

    Update app.js
    working

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var userRouter = require('./routes/user');
+
 var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
 
@@ -24,9 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/user', userRouter);
+
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 
